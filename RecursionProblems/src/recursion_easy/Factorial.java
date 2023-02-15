e54d9e80535fe9caea098488a25ef9ccb83bfd91
Shubhangi-Srivastava <srivastava.sh@northeastern.edu>
2023-02-15 15:29:26 -0500

    Added factorial of a number code

---
+++ RecursionProblems/src/recursion_easy/Factorial.java
@@ -0,0 +1,18 @@
+/** 
+ * Author: Shubhangi Srivastava
+ * Factorial of a number
+ */
+package recursion_easy;
+
+public class Factorial {
+    public static void main(String[] args){
+       int ans = fact(5);
+       System.out.println(ans);
+    }
+    static int fact(int n){ 
+        if(n <= 1){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+}
